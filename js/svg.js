08fc82e8df8e5822595ee5bc329b982a1fe3743f
eli <eli.sakov@hotmail.com>
2014-07-03 15:09:03 -0700

    draggable

--- js/svg.js
+++ js/svg.js
@@ -1,3 +1,17 @@
-define(["vendor/svg"], function (argument) {
-  return SVG("workspace").height("90%");
+define(["vendor/svg", "js/vendor/jquery-1.10.2.min.js"], function (argument) {
+ 	var svg = SVG("workspace").height("1000%");
+	$("#workspace svg").on("mousedown", function(e) {
+		e.stopPropagation()
+		var x = e.clientX, y = e.clientY,
+		svg = $("#workspace svg").css("cursor", "move")[0];
+		document.onmousemove = (function(e) {
+			e.stopPropagation()
+			svg.style.bottom = y - e.clientY;
+			svg.style.right = x - e.clientX;
+		}).bind(this)
+	}).on("mouseup", function(){
+		document.onmousemove = null;
+		$("#workspace svg").css("cursor", "inherit")
+	});
+	return svg;
 });
